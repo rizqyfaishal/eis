25bb064ddafe74b77249bc3b8245ed5bc7c9148d
cymon1997 <cybermonster82@yahoo.com>
2016-08-22 11:37:17 +0700

    no message

--- Project_EIS/app/Http/routes.php
+++ Project_EIS/app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/old', function () {
+    return view('home_old');
+});
