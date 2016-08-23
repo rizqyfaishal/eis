a6cd4a3d74d5ae6a78aaed627b5f640712001fdc
cymon1997 <cybermonster82@yahoo.com>
2016-08-23 19:38:40 +0700

    addressing

--- Project_EIS/app/Http/routes.php
+++ Project_EIS/app/Http/routes.php
@@ -19,6 +19,26 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/meet-eis', function () {
+    return view('meet-eis');
+});
+
+Route::get('/our-program', function () {
+    return view('our-program');
+});
+
+Route::get('/contact-us', function () {
+    return view('contact-us');
+});
+
+Route::get('/ask-our-alumni', function () {
+    return view('ask-our-alumni');
+});
+
+Route::get('/social-media', function () {
+    return view('social-media');
+});
+
 Route::get('/old', function () {
     return view('home_old');
 });
--- Project_EIS/resources/views/home.blade.php
+++ Project_EIS/resources/views/home.blade.php
@@ -24,12 +24,12 @@
         <div class="row nav-bar">
             <nav role="navigation">
                 <ul class="nav header-ul">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Meet EIS</a></li>
-                    <li><a href="#">Our Program</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Ask Our Alumni</a></li>
-                    <li><a href="#">Social Media</a></li>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/meet-eis">Meet EIS</a></li>
+                    <li><a href="/our-program">Our Program</a></li>
+                    <li><a href="/contact-us">Contact Us</a></li>
+                    <li><a href="/ask-our-alumni">Ask Our Alumni</a></li>
+                    <li><a href="/social-media">Social Media</a></li>
                 </ul>
             </nav>
         </div>
