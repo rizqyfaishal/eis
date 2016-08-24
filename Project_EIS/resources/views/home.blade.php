53b2c0ffbafb98bc71e46a7c7b14883dde4e1ac7
cymon1997 <cybermonster82@yahoo.com>
2016-08-24 19:18:43 +0700

    no message

--- Project_EIS/resources/views/home.blade.php
+++ Project_EIS/resources/views/home.blade.php
@@ -21,18 +21,9 @@
             <img class="" src="{{ URL::asset('img/EIS_logo_text.gif') }}">
         </div>
 
-        <div class="row nav-bar">
-            <nav role="navigation">
-                <ul class="nav header-ul">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/meet-eis">Meet EIS</a></li>
-                    <li><a href="/our-program">Our Program</a></li>
-                    <li><a href="/contact-us">Contact Us</a></li>
-                    <li><a href="/ask-our-alumni">Ask Our Alumni</a></li>
-                    <li><a href="/social-media">Social Media</a></li>
-                </ul>
-            </nav>
-        </div>
+        @yield('nav-bar')
+
+
     </header>
 
     <div class="row whole-content">
@@ -70,8 +61,8 @@
             <img src="{{ URL::asset('img/eis_mark.png') }}">
         </div>
 
-        <div class="row">
-            <p>Copyright &copy; 2016 by <a href="http://fukicorp.id">Fuki Corp</a></p>
+        <div class="footer-copyright">
+            <p>Copyright &copy; 2016 by <a href="http://fukicorp.id">FUKI Corp</a></p>
         </div>
     </footer>
 
---
+++ Project_EIS/resources/views/layout/nav-bar.blade.php
@@ -0,0 +1,15 @@
+
+@section('nav-bar')
+    <div class="row nav-bar">
+        <nav role="navigation">
+            <ul class="nav header-ul">
+                <li><a href="/home">Home</a></li>
+                <li><a href="/meet-eis">Meet EIS</a></li>
+                <li><a href="/our-program">Our Program</a></li>
+                <li><a href="/contact-us">Contact Us</a></li>
+                <li><a href="/ask-our-alumni">Ask Our Alumni</a></li>
+                <li><a href="/social-media">Social Media</a></li>
+            </ul>
+        </nav>
+    </div>
+@stop
