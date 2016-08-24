8c5f5785e56636aa1b4bdf3ab6c74514263714af
maula <maulafaiz97@gmail.com>
2016-08-24 20:38:27 +0700

    contact-us

---
+++ Project_EIS/resources/views/contact-us.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <title>Contact Us</title>
+</head>
+    <body>
+        <div class="row text-center font-avenir-book">
+            <h1>CONTACT US</h1>
+        </div>
+        <div class="row">
+
+            <iframe id="maps"
+  width="600"
+  height="450"
+  frameborder="200px" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAyl5lSDTppcIvICxImY-oDCHyt0dDRdkE
+    &q=Fakultas Ekonomi dan Bisnis Universitas Indonesia" >
+</iframe>
+
+        </div>
+        <div class="row text-center font-avenir-book">
+            <div class="row">
+                <p>Gedung Prof. Mr. R. Soenario Kolopaking, 2nd Floor</p>
+            </div>
+            <div class="row">
+                <p>Jalan Prof. Dr. Soemitro Djojohadikusumo</p>
+            </div>
+            <div class="row">
+                <p>Depok 16424</p>
+            </div>
+            <div class="row">
+                <p>(021) 727-2425 ext. 134, 144</p>
+            </div>
+            <div class="row">
+                <p>eisociety.ui@gmail.com</p>
+            </div>
+        </div>
+    </body>
+</html>
