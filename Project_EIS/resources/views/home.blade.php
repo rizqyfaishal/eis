a2ede8aaaff4bd90e07676bf01a14e2dbc4fe785
LUTHVIAR\luthv <luthviar.a@gmail.com>
2016-08-25 21:31:54 +0700

    embed instagram

--- Project_EIS/resources/views/home.blade.php
+++ Project_EIS/resources/views/home.blade.php
@@ -17,6 +17,14 @@
             <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
 
+        <div>
+            <!-- SnapWidget -->
+            <br>
+            <h4>instagram</h4>
+            <script src="https://snapwidget.com/js/snapwidget.js"></script>
+            <iframe src="https://snapwidget.com/embed/242414" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
+        </div>
+
         {{--<article class="article">--}}
             {{--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget venenatis leo. Pellentesque luctus justo ultrices metus eleifend semper. Ut volutpat congue est non mollis. Sed dignissim massa vel condimentum tempus. Integer suscipit ex vel pellentesque eleifend. Vivamus sit amet sapien ac mi ultricies varius. Nulla facilisi. Proin lacinia dui nec ligula iaculis, eget gravida dolor tempus. Nam sed ipsum scelerisque, dignissim mauris eget, tempor tellus. Aenean quis diam feugiat, condimentum lorem in, dictum libero. Suspendisse at ex sed risus semper pulvinar a nec urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut fringilla quis velit quis malesuada. Donec vulputate malesuada placerat. Phasellus cursus, massa ac blandit placerat, est dui ultrices nunc, vitae porta mi dui nec tortor. Proin leo risus, luctus a luctus ac, fringilla nec est.</p>--}}
         {{--</article>--}}
@@ -31,6 +39,7 @@
                 <a href="#"><h2 class="title-bold"><b>JAKARTA LIKE NEW YO..</b></h2></a>
                 <a href="#"><h2 class="title-bold"><b>NEW YORK AT UI HAS...</b></h2></a>
                 <a href="#"><h2 class="title-bold"><b>NEW YORK AT UI HAS...</b></h2></a>
+                <button class="btn btn-primary">Load more..</button>
             </div>
         </div>
 
@@ -47,6 +56,7 @@
                 <a href="#"><h3 class="title-bold"><b>JAKARTA LIKE NEW YO..</b></h3></a>
                 <a href="#"><h3 class="title-bold"><b>NEW YORK AT UI HAS...</b></h3></a>
                 <a href="#"> <h3 class="title-bold"><b>NEW YORK ON CITY</b></h3></a>
+                <button class="btn btn-primary">Load more..</button>
             </div>
         </div>
         {{--<article class="article">--}}
